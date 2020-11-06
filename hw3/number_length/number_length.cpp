10c9b756cdfaf80dbecab0045500562e20d7de07
miktuy <miktuy@mail.ru>
2020-11-06 19:07:50 +0300

    improved loop
    Change-Id: Icfd0c1eabfc87ee12f69190552ff21b68006d925

--- hw3/number_length/number_length.cpp
+++ hw3/number_length/number_length.cpp
@@ -3,13 +3,12 @@
 int main()
 {
     int number = 0;
-    int number_length = 0;
+    int number_length = 1;
     printf("Enter number: ");
     scanf("%d", &number);
     printf("Length of `%d` is ", number);
-    while (number > 0)
+    while ((number /= 10) > 0)
     {
-        number /= 10;
         ++number_length;
     }
     printf("`%d`\n", number_length);
